5a44b63ecd2a7a680e7be7eff7a32541ed1ff015
Edgar Silva <edgar.osc@gmail.com>
2014-12-05 15:17:06 -0600

    Add preinstall script for xpc-connection on OSX.

---
+++ scripts/preinstall.js
@@ -0,0 +1,13 @@
+var cp = require('child_process');
+
+if (process.platform === 'darwin') {
+   cp.exec("npm install git://github.com/hybridgroup/node-xpc-connection.git#pre-compiled-binaries", function(err) {
+     if  (!!err){
+       console.log('An error occurred: ', err);
+       console.log('Try installing xpc-connection module manually by running:');
+       console.log("\"npm install git://github.com/hybridgroup/node-xpc-connection.git#pre-compiled-binaries\"",);
+     } else {
+       console.log("xpc connection module has been installed!");
+     }
+   });
+}
